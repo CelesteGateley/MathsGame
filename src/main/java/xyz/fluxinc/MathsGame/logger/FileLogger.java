110312b96540cd7924e527b96112a4260b5be59b
Celeste Gateley <celeste.gateley@gmail.com>
2021-02-18 01:04:35 +0000

    Updated logging errors to look better inside a file

--- src/main/java/xyz/fluxinc/MathsGame/logger/FileLogger.java
+++ src/main/java/xyz/fluxinc/MathsGame/logger/FileLogger.java
@@ -10,90 +10,69 @@ import static xyz.fluxinc.MathsGame.logger.LogLevel.compareLevel;
 
 public class FileLogger extends Logger{
 
-    private final File logFile;
+    private final PrintStream printStream;
 
-    public FileLogger(File logFile) {
-        this.logFile = logFile;
-        try { logFile.createNewFile(); }
-        catch (IOException e) { e.printStackTrace();}
+    public FileLogger(File logFile) throws LoggingFailedException {
+        try {
+            logFile.createNewFile();
+            printStream = new PrintStream(new FileOutputStream(logFile, true));
+        } catch (IOException e) {
+            throw new LoggingFailedException("Failed to initialise output stream");
+        }
     }
 
     @Override
-    public void debug(String message) throws LoggingFailedException {
+    public void debug(String message) {
         if (!compareLevel(LogLevel.DEBUG, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [DEBUG] " + message);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [DEBUG] " + message);
+        printStream.flush();
     }
 
     @Override
     public void info(String message) throws LoggingFailedException {
         if (!compareLevel(LogLevel.INFO, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [INFO] " + message);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [INFO] " + message);
+        printStream.flush();
     }
 
     @Override
     public void warn(String message) throws LoggingFailedException {
         if (!compareLevel(LogLevel.WARN, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [WARN] " + message);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [WARN] " + message);
+        printStream.flush();
     }
 
     @Override
     public void error(String message) throws LoggingFailedException {
         if (!compareLevel(LogLevel.ERROR, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [ERROR] " + message);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [ERROR] " + message);
+        printStream.flush();
     }
 
     @Override
     public void fatal(String message) throws LoggingFailedException {
         if (!compareLevel(LogLevel.FATAL, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [FATAL] " + message);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [FATAL] " + message);
+        printStream.flush();
     }
 
     @Override
     public void error(Exception e) throws LoggingFailedException {
         if (!compareLevel(LogLevel.ERROR, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [ERROR] An error has occurred, details are below");
-            e.printStackTrace(printStream);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [ERROR] An error has occurred, details are below");
+        printStream.println(new String(new char[120]).replace("\0", "-"));
+        e.printStackTrace(printStream);
+        printStream.println(new String(new char[120]).replace("\0", "-"));
+        printStream.flush();
     }
 
     @Override
     public void fatal(Exception e) throws LoggingFailedException {
         if (!compareLevel(LogLevel.FATAL, MathsGame.LOG_LEVEL)) return;
-        try {
-            PrintStream printStream = new PrintStream(new FileOutputStream(logFile, true));
-            printStream.println(dateFormat.format(new Date()) + " [FATAL] A fatal error has occurred, details are below");
-            e.printStackTrace(printStream);
-            printStream.flush();
-            printStream.close();
-        } catch (FileNotFoundException err) { throw new LoggingFailedException(err); }
+        printStream.println(dateFormat.format(new Date()) + " [FATAL] A fatal error has occurred, details are below");
+        printStream.println(new String(new char[120]).replace("\0", "-"));
+        e.printStackTrace(printStream);
+        printStream.println(new String(new char[120]).replace("\0", "-"));
+        printStream.flush();
     }
 }
