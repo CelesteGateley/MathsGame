77996f537f2c63f6fc5480a42fcd6dab115478cd
Celeste Gateley <celeste.gateley@gmail.com>
2021-02-18 01:06:40 +0000

    Added difference between error and fatal boxing

--- src/main/java/xyz/fluxinc/MathsGame/MathsGame.java
+++ src/main/java/xyz/fluxinc/MathsGame/MathsGame.java
@@ -1,5 +1,6 @@
 package xyz.fluxinc.MathsGame;
 
+import com.sun.istack.internal.Nullable;
 import xyz.fluxinc.MathsGame.errors.LoggingFailedException;
 import xyz.fluxinc.MathsGame.errors.StorageFailedException;
 import xyz.fluxinc.MathsGame.logger.FileLogger;
@@ -11,17 +12,23 @@ import java.io.File;
 public class MathsGame {
 
     public static LogLevel LOG_LEVEL = LogLevel.DEBUG;
-    private static Logger logger = new FileLogger(new File("latest.log"));
+    private static Logger logger;
+    private static Session currentSession;
+    public static int MAX_QUESTIONS = 10;
 
     public static void main(String[] args) throws LoggingFailedException {
+        logger = new FileLogger(new File("latest.log"));
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warning message");
         logger.error("This is a error message");
         logger.fatal("This is a fatal message");
         logger.error(new StorageFailedException("This is a non-fatal exception"));
-        //logger.fatal(new StorageFailedException("This is a fatal exception"));
+        logger.fatal(new StorageFailedException("This is a fatal exception"));
     }
 
     public static Logger getLogger() { return logger; }
+
+    @Nullable
+    public static Session getSession() { return currentSession; }
 }
